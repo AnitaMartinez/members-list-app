c8c05bc0508c77f487693446162ec23a5a3831bd
AnitaMartinez <amartinezaguilar6@gmail.com>
2019-11-23 18:36:32 +0100

    Back: Add files from database folder

---
+++ back/database/index.js
@@ -0,0 +1,43 @@
+const MongoClient = require('mongodb').MongoClient
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'teamMembers';
+
+const saveInBD = data => {
+    MongoClient.connect(url, (err, client) => {
+        if(!err){
+            const col = client.db(dbName).collection('members')
+            col.deleteMany({}, (err, result) => { if(err) console.log(err) })
+            col.insertMany(data, (err, result) => {if(err) console.log(err)}) 
+            client.close()
+        }
+    })
+}
+
+const getMembersFromBD = (pageNumber, pageSize) => {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, (err, client) => {
+            if(!err){
+                const col = client.db(dbName).collection('members')
+                const options = {
+                    "limit": pageSize,
+                    "skip": pageNumber * pageSize
+                }
+                col.find({}, options).toArray((err, members) => {
+                    if (err) {
+                        console.log('ERROR reading from database', err.message, err.data)
+                        reject('Error', err)
+                    } else {
+                        resolve({members, pagination: {pages: 1}})  // TODO: pages: itemsNumber / page_sizes
+                    }
+                })
+                client.close()
+            }
+        })
+    })
+}
+
+module.exports = {
+    saveInBD,
+    getMembersFromBD
+}
